3205a67899e82eb42bd8567f4282d3ad63125339
LazyBrush <andy@winton.net>
2024-01-06 12:10:09 +0000

    press space bar

---
+++ scenes/cart/cart.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		print("space bar")
+		apply_central_impulse(Vector2(0,-300))
+	
